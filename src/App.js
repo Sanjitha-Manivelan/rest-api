349809244e9b1da731ea65ac61dfe5335b4f64c5
msanj <msanjitha.shanthi@gmail.com>
2023-01-28 08:09:00 -0800

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef} from "react";
+import TodoList from "./Todolist.js";
+import {v4 as uuidv4} from "uuid";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  function toggleTodos(id){
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id == id);
+    todo.complete = !todo.complete;
+    setTodos(newTodos);
+  }
+
+  function addTodo(e) {
+    const name = todoNameRef.current.value;
+    todoNameRef.current.value = null;
+    if (name == "") return;
+    setTodos((prevtodo) => {
+      return [...prevtodo, {id: uuidv4(), name: name, complete: false}];
+    })
+  }
+
+  function clearTodo(e) {
+    const newTodos = todos.filter((todo) => !todo.complete);
+    setTodos(newTodos);
+  }
+
+  return(
+    <>
+    <TodoList todos={todos} toggleTodo={toggleTodos} />
+    <input ref={todoNameRef} type="text" />
+    <button onClick={addTodo}>Add Hero</button>
+    <button onClick={clearTodo}>Clear Heroes</button>
+    <div>{todos.filter((todo) => !todo.complete).length} number of heroes</div>
+    </>
   );
 }
 
